b1bca4093c528816cc4d50f927e2bbf790d97838
mistaken-identity <antonyteflon10@gmail.com>
2026-01-08 12:42:42 +0300

    Fix order route in api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "http://localhost:5000/api/orders";
 
 export const createOrder = async (orderData: any) => {
     return axios.post(`${API_URL}/orders`, orderData);
